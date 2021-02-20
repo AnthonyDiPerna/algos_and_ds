726de8b957bd88bc947d8dbdc99a42671121dc73
TonyD <anthonydiperna@gmail.com>
2021-02-20 16:00:49 -0500

    add in 2_2_2 big -> small

--- 2_1_1_insertion_sort.c
+++ 2_1_1_insertion_sort.c
@@ -1,14 +1,14 @@
-#define MAX (6)
+#define SIZE (6)
 
 //Sort the array with insertion sort
-int arr[MAX] = {31,41,59,26,41,58};
+int arr[SIZE] = {31,41,59,26,41,58};
 
 void insertion_sort(int * arr, int len)
 {
-  for (int i = 1; i <= MAX-1; i++)
+  for (int i = 1; i <= SIZE-1; i++)
   {
     int j = i;
-    //If current index arr[i] > prev index arr[i-1] swap position
+    //If current index arr[i] < prev index arr[i-1] swap position
     while (j > 0 && arr[j] < arr[j-1])
     {
       //swap this value back a spot
@@ -20,6 +20,23 @@ void insertion_sort(int * arr, int len)
   }  
 }
 
+//2.2.2 BIGGEST to SMALLEST
+void insertion_sort_biggest_to_smallest(int * arr, int len)
+{
+    for (int i = 1; i <= SIZE-1; i++)
+  {
+    int j = i;
+    //If current index arr[i] > prev index arr[i-1] swap position
+    while (j > 0 && arr[j] > arr[j-1])
+    {
+      //swap this value back a spot
+      int tmp = arr[j-1];
+      arr[j-1] = arr[j];
+      arr[j] = tmp;
+      j--;
+    }
+  }  
+}
 
 //output
 //INSERTION SORT
@@ -29,13 +46,32 @@ void insertion_sort(int * arr, int len)
 //   index 3 value 41
 //   index 4 value 58
 //   index 5 value 59
+
+/*
+INSERTION SORT - BIGGEST TO SMALLEST
+   index 0 value 59
+   index 1 value 58
+   index 2 value 41
+   index 3 value 41
+   index 4 value 31
+   index 5 value 26
+*/
 int main() {     
  	
-   insertion_sort(arr,MAX);
+   insertion_sort(arr,SIZE);
    
    printf("\nINSERTION SORT");
    
-   for (int i = 0; i < MAX; i++)
+   for (int i = 0; i < SIZE; i++)
+   {
+     printf("\n   index %d value %d",i,arr[i]);
+   }
+
+  insertion_sort_biggest_to_smallest(arr,SIZE);
+
+  printf("\nINSERTION SORT - BIGGEST TO SMALLEST");
+   
+   for (int i = 0; i < SIZE; i++)
    {
      printf("\n   index %d value %d",i,arr[i]);
    }
